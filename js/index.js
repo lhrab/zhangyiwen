7051f81b44a8043b55e8eb25f3b046a8e357ddd5
lhrab <1219237545@qq.com>
2021-11-11 17:16:51 +0800

    第五周作业

--- js/index.js
+++ js/index.js
@@ -18,5 +18,27 @@ $(function() {
         controlsCurrentColor: "#ff6600", //当前控制按钮的颜色
         isShowNum: true //是否显示数字
     });
+    /* 图书电子书小轮播 */
+    $('#ebooks-banner').tyslide({
+        boxh: 226, //盒子的高度
+        w: 332, //盒子的宽度
+        h: 226, //图片的高度
+        isShow: true, //是否显示控制器
+        isShowBtn: true, //是否显示左右按钮
+        controltop: 5, //控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW: 20, //控制按钮宽度
+        controlsH: 2, //控制按钮高度
+        controlsColor: "#d7d7d7", //普通控制按钮的颜色
+        controlsCurrentColor: "#00ff00", //当前控制按钮的颜色
+    });
+    /* 新书列表手风琴效果 */
+    $('.ebooks .right-box ul > li').mouseenter(function() {
+        //所有兄弟：隐藏详情 显示标题
+        $(this).siblings().find('.desc').hide(); //隐藏详情
+        $(this).siblings().find('.eboos-title').show(); //显示标题
 
+        //当前：隐藏标题 显示详情
+        $(this).find('.eboos-title').hide(); //隐藏标题
+        $(this).find('.desc').show(); //显示详情
+    });
 });
