064bd405f63a9245feb4a0e40cd70a7961ebd317
lhrab <1219237545@qq.com>
2021-11-13 14:41:15 +0800

    第六周作业

--- js/index.js
+++ js/index.js
@@ -41,4 +41,43 @@ $(function() {
         $(this).find('.eboos-title').hide(); //隐藏标题
         $(this).find('.desc').show(); //显示详情
     });
+    /* 服装小轮播 */
+    $('#clothes-banner1').tyslide({
+        boxh: 334, //盒子的高度
+        w: 440, //盒子的宽度
+        h: 334, //图片的高度
+        isShow: true, //是否显示控制器
+        isShowBtn: true, //是否显示左右按钮
+        controltop: 5, //控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW: 20, //控制按钮宽度
+        controlsH: 2, //控制按钮高度
+        controlsColor: "#d7d7d7", //普通控制按钮的颜色
+        controlsCurrentColor: "#ff6600", //当前控制按钮的颜色
+    });
+    /* 户外运动小轮播 */
+    $('#sport-banner1').tyslide({
+        boxh: 334, //盒子的高度
+        w: 440, //盒子的宽度
+        h: 334, //图片的高度
+        isShow: true, //是否显示控制器
+        isShowBtn: true, //是否显示左右按钮
+        controltop: 5, //控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW: 20, //控制按钮宽度
+        controlsH: 2, //控制按钮高度
+        controlsColor: "#d7d7d7", //普通控制按钮的颜色
+        controlsCurrentColor: "#ff6600", //当前控制按钮的颜色
+    });
+    /* 童装小轮播 */
+    $('#children-clothes-banner1').tyslide({
+        boxh: 334, //盒子的高度
+        w: 440, //盒子的宽度
+        h: 334, //图片的高度
+        isShow: true, //是否显示控制器
+        isShowBtn: true, //是否显示左右按钮
+        controltop: 5, //控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW: 20, //控制按钮宽度
+        controlsH: 2, //控制按钮高度
+        controlsColor: "#d7d7d7", //普通控制按钮的颜色
+        controlsCurrentColor: "#ff6600", //当前控制按钮的颜色
+    });
 });
